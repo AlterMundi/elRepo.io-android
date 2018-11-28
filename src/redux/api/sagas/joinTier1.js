88dbecba3ce50fb9192be444d80f169ac9666512
Marcos Gutierrez <gmarcos87@gmail.com>
2018-11-28 16:17:53 -0300

    Fix cert parse in tier1 sagas

--- src/redux/api/sagas/joinTier1.js
+++ src/redux/api/sagas/joinTier1.js
@@ -1,6 +1,8 @@
 import { takeEvery, put } from 'redux-saga/effects'
 import httpApi from '../../../httpApi';
+import { apiCall } from '../../../helpers/apiWrapper';
 import config from '../../../config';
+import { store } from '../../store';
 
 const apiHttp = httpApi(config.api.url,config.api.port);
 
@@ -8,30 +10,41 @@ export const joinTiers = function*() {
     yield takeEvery('JOIN_TIER', function*(action){
         console.log(action.payload.cert.replace(/\n/g,'\\n'))
         try {
-            const result = yield fetch(action.payload.url, {
+            const result = yield fetch(action.payload.url+ '/rsPeers/acceptInvite', {
                 body: JSON.stringify({
-                    invite: action.payload.cert.replace(/\n/g,'\\n')
+                    invite: action.payload.cert
                 }),
                 headers: {'content-type': 'application/json'},
                 method: 'POST'
             }).then(res => res.json())
 
-            yield put({type: 'JOIN_TIER_SUCCESS', payload: result });
+            yield put({type: 'JOIN_TIER_SUCCESS', payload: { result, url: action.payload.url }});
         } catch(e) {
             yield put({type: 'JOIN_TIER_FAILD', payload: { error: e, dataSend: action.payload.cert}});
         }
     });
     
-    yield takeEvery (['JOIN_TIER_SUCCESS','ADD_FRIEND'], function*({type, payload = {}}){
+    yield takeEvery (['JOIN_TIER_SUCCESS'], function*({type, payload = {}}){    
         try {
-            yield  apiHttp.send('api', {
-                type: type,
-                payload: {
-                    path: '/rsPeers/acceptInvite',
-                    data: {
-                        invite: payload.cert || config.tiers1[0].credential
-                    }
-                }
+            const result = yield fetch(payload.url+'/rsPeers/GetRetroshareInvite', {
+                body: JSON.stringify({}),
+                headers: {'content-type': 'application/json'},
+                method: 'POST'
+            }).then(res => res.json())
+            console.log({result})
+            const acceptInvite = yield apiCall('ADD_TIER_ONE','/rsPeers/acceptInvite',{
+                invite: result.retval
+            })
+            yield put({type: 'GET_TIER_INVITE_SUCCESS', payload: acceptInvite});
+        } catch(e) {
+            yield put({type: 'GET_TIER_INVITE_FAILD', payload: { error: e, dataSend: action.payload.cert}});
+        }
+    });
+    
+    yield takeEvery (['ADD_FRIEND'], function*({type, payload = {}}){
+        try {
+            const result = yield apiCall(type,'/rsPeers/acceptInvite',{
+                invite: payload.cert.replace(/\n/g,'\\n')
             })
         } catch(e) {
             console.warn('Wrong cert', e, payload)

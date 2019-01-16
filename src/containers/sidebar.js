d22bd67949eecf075dc6ccb6b484fd2ea2c58ce3
Marcos Gutierrez <gmarcos87@gmail.com>
2019-01-16 16:01:57 -0300

    Change sidebar status

--- src/containers/sidebar.js
+++ src/containers/sidebar.js
@@ -8,28 +8,33 @@ import config from '../config';
 
 
 const DrawerItemsData = [
-  { label: 'Publicaciones', icon: 'inbox', key: 0, goTo: 'elRepoIO.home' },
-  { label: 'Descargas', icon: 'folder-shared', key: 4, goTo: 'elRepoIO.fileList'},
-  { label: 'Buscar', icon: 'search', key: 1 , goTo: 'elRepoIO.search'},
-  { label: 'Publicar', icon: 'file-upload', key: 2, goTo: 'elRepoIO.upload'},
-  { label: 'Estado de red', icon: 'settings-input-antenna', key: 3 , goTo: 'elRepoIO.status'}
+  { label: 'Publicaciones', icon: 'inbox', key: 'key-0', goTo: 'elRepoIO.home' },
+  { label: 'Descargas', icon: 'folder-shared', key: 'key-4', goTo: 'elRepoIO.fileList'},
+  { label: 'Buscar', icon: 'search', key: 'key-1' , goTo: 'elRepoIO.search'},
+  { label: 'Publicar', icon: 'file-upload', key: 'key-2', goTo: 'elRepoIO.upload'},
+  { label: 'Estado de red', icon: 'settings-input-antenna', key: 'key-3' , goTo: 'elRepoIO.status'}
 ];
 
 class DrawerItems extends React.Component {
-  state = {
-    open: false,
-    drawerItemIndex: 0,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      drawerItemIndex: 'key-0',
+    };
+    this._setDrawerItem = this._setDrawerItem.bind(this)
+  }
+
 
   _setDrawerItem = (index,goTo) => {
-      this.setState({ drawerItemIndex: index })  
+     this.setState({ drawerItemIndex: index })
       Navigation.setStackRoot('App',{
         sideMenu: {
           center: { 
             stack: {
               children: [{
               component: {
-                  name: goTo,
+                  name: goTo
               }
             }]
           }
@@ -52,8 +57,6 @@ class DrawerItems extends React.Component {
           {DrawerItemsData.map((props) => (
             <Drawer.Item
               {...props}
-              key={props.key}
-              
               active={this.state.drawerItemIndex === props.key}
               onPress={() => this._setDrawerItem(props.key, props.goTo)}
             />

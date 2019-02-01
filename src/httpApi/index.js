ce9e92545d0b42b816faba09219a8dc397dd2133
Marcos Gutierrez <gmarcos87@gmail.com>
2019-02-01 15:18:15 -0300

    Make API calls synchronously

--- src/httpApi/index.js
+++ src/httpApi/index.js
@@ -1,11 +1,48 @@
 import { store } from '../redux/store';
 import EventSource from './events';
 import Base64 from '../helpers/base64';
+import uuid from 'uuid/v4'
 
+let requestList = [];
 const api = (url, port) => {
-    const apiHttp = (path, data, method, headers) => fetch(url+':'+port+path, { headers, method , body: data? JSON.stringify(data): undefined}   )
-                //.then(result => { console.log(result); return result;})
-                .then(result => result.json())
+
+    const logRequest = (type, data) => {
+        //Only for debug
+        //console.log('REQUEST', type, data)
+    }
+
+    const pullRequest = ()  => {
+        if(requestList.length > 0) {
+            requestList[0].callRequest()
+        }
+    }
+    const apiHttp = (path, data, method, headers) => new Promise((res, rej) => { 
+        const id = uuid();
+        logRequest('NEW:  ', id, requestList)
+        requestList = [...requestList, { id, callRequest : () => {
+            logRequest('CALLING', {path, data, method, headers})
+            fetch(url+':'+port+path, { headers, method , body: data? JSON.stringify(data): undefined}   )
+                 //.then(result => { console.log(result); return result;})
+                .then(result => { 
+                    requestList = requestList.filter(x => x.id !== id)
+                    return  result.json()
+                })
+                .then(data => {
+                    res(data)
+                    logRequest('RESULT', {id, data})
+                    pullRequest()
+                })
+                .catch(e => {
+                    rej(e)
+                    requestList = requestList.filter(x => x.id !== id)
+                    pullRequest()
+                })
+        }}]
+
+        if(requestList.length === 1) {
+            requestList[0].callRequest()
+        }
+    })
 
     return  {
         send: (version, request) =>  {
